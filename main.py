bb02332dda76ec644410fa8a6dcbc9e8f9087be8
londony11 <londony11@gmail.com>
2023-11-16 07:07:14 +0100

    Put everython to class

--- main.py
+++ main.py
@@ -1,60 +1,78 @@
 """TypeSpeedTest Desktop App"""
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from data import EXAMPLE
 
 
-class TypeTest:
+class Windows(tk.Tk):
     """Application class."""
 
-    def __init__(self, root, text, user_input):
+
+    def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+
+        self.text_to_type = tk.StringVar(value=EXAMPLE)
+        self.user_input = tk.StringVar()
 
         # Title
-        root.title("Type speed test")
+        self.title("Type speed test")
 
         # Frame
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        self.mainframe = ttk.Frame(self, padding="3 3 12 12")
+        self.mainframe.grid(column=0, row=0, sticky="nwes")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
         # Text for text tp write
-        ttk.Label(mainframe, text="Text to type:").grid(column=0, row=0)
-        text_window = Text(mainframe, height=5, width=35, wrap="word")
-
-        # Letter options
-        text_window.tag_configure("default", font="Arial 12")
-        text_window.tag_configure("next", font="Arial 14 bold", foreground="green")
-        text_window.tag_configure("correct", font="Arial 12", background="green")
-
-        # Insert default text
-        text_window.insert("end", text.get()[0], "next")
-        text_window.insert("end", text.get()[1:], "default")
-        text_window.configure(state="disabled")
-        text_window.grid(column=0, row=1, sticky=(W, E))
+        ttk.Label(self.mainframe, text="Text to type:").grid(column=0, row=0)
+        self.text_window = tk.Text(self.mainframe, height=5, width=35, wrap="word")
+        self.text_window.configure(state="disabled")
+        self.text_window.grid(column=0, row=1, sticky="we")
 
         # Text entry
-        ttk.Label(mainframe, text="User input:").grid(column=0, row=2)
-        user_input_window = ttk.Entry(mainframe, textvariable=user_input, width=35)
-        user_input_window .grid(column=0, row=3, sticky=(W, E))
+        ttk.Label(self.mainframe, text="User input:").grid(column=0, row=2)
+        self.user_input_window = ttk.Entry(self.mainframe, textvariable=self.user_input, width=35)
+        self.user_input_window .grid(column=0, row=3, sticky="we")
 
-        for child in mainframe.winfo_children():
+        # Add padding to widget
+        for child in self.mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
-        user_input_window.focus()
+        self.user_input_window.focus()
+
+    def show_text_to_type(self):
+        """Show text function"""
+
+        # Enable window
+        self.text_window.configure(state="normal")
+
+        # Letter options
+        self.text_window.tag_configure("default", font="Arial 12")
+        self.text_window.tag_configure("next", font="Arial 14 bold", foreground="green")
+        self.text_window.tag_configure("correct", font="Arial 12", background="green")
+
+        # Insert default text
+        self.text_window.insert("end", self.text_to_type.get()[0], "next")
+        self.text_window.insert("end", self.text_to_type.get()[1:], "default")
+
+        # Disable window
+        self.text_window.configure(state="disabled")
 
 
-    def check_text(self, *args):
+    def check_text(self):
         """Check user input."""
-        # for count, char in enumerate(user_input.get()):
-        #     if char == text.get()[count]:
-        #         pass
-        
+        for count, char in enumerate(self.user_input.get()):
+            if char == self.text_to_type.get()[count]:
+                print("Hello")
 
 
-root = Tk()
-text = StringVar(value=EXAMPLE)
-user_input = StringVar()
-app = TypeTest(root, text, user_input)
-user_input.trace_add("write", TypeTest.check_text)
-root.mainloop()
+def main():
+    """Main function"""
+    app = Windows()
+    app.show_text_to_type()
+    app.check_text()
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
